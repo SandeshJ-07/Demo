327e70ef305b31b5a47bf3f5231779c787ddaf77
Sandesh Jain <sandeshjain2507@gmail.com>
2024-02-06 22:58:57 +0530

    Nye Commit daale h

---
+++ components/let/A.tsx
@@ -0,0 +1,7 @@
+const A = () => {
+    return(
+        <div>Sample Component A2</div>
+    )
+}
+
+export default A;
--- pages/sample.tsx
+++ pages/sample.tsx
@@ -1,10 +1,12 @@
 import A from "@/components/A";
+import A2 from "@/components/let/A";
 
 const SampleComponent = () => {
     return(
         <div>
             <p>Sample Code H Y!</p>
             <A/>
+            <A2/>
             </div>
     );
 }
