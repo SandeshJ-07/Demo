74eebf7d141235b8ffb7b332d37114fb1c2b62f0
Sandesh Jain <sandeshjain2507@gmail.com>
2024-02-07 22:44:58 +0530

    plugin added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+import WebpackReactComponentNamePlugin from 'webpack-react-component-name';
+
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, {isServer}) => {
+    if(!isServer){
+      config.plugins.push(
+        new WebpackReactComponentNamePlugin()
+        )
+    }
     config.devtool = 'cheap-module-source-map';
     return config;
   }
